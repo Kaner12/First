a2709ceb1fd3b6a8d5ce92d48bdfce187217177a
Kaner12 <133087763+Kaner12@users.noreply.github.com>
2023-05-10 11:19:56 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Git
+{
+    class Program
+    {
+
+
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Введите число");
+            string t=Console.ReadLine();
+        }
+    }
+}
